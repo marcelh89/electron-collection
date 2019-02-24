ad60016222a083297bb1ce5bc6c1533ba2c15df1
Marcel Hinderlich <marcelh89@googlemail.com>
2019-02-24 20:12:40 +0100

    chat-app: using React

--- chat-app/src/renderer.js
+++ chat-app/src/renderer.js
@@ -1,5 +1,32 @@
-const io = require('socket.io-client');
+/*const io = require('socket.io-client');
 
 let socket = io('http://localhost:3000');
 
-socket.emit("newMessage", "Hello There");
+socket.emit("newMessage", "Hello There");*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+let root = document.getElementById("root");
+
+document.addEventListener("DOMContentLoaded", e => {
+    ReactDOM.render(<TestComponent />, root);
+
+});
+
+class TestComponent extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        return (
+            <div>
+                <h3>Hello from the other side</h3>
+            </div>
+        );
+
+    }
+}
+
